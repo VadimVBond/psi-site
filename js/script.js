7a5412014eb4d163a7e1cddca8be9ec8d1e9e6ee
vadimb <seowebdes@mail.ru>
2022-02-08 11:33:27 +0200

    christmas

--- js/script.js
+++ js/script.js
@@ -1,29 +1,39 @@
-let navbar = document.querySelector('.navbar');
+let searchBtn = document.querySelector('#search-btn');
+let searchForm = document.querySelector('.header .search-form');
 
-document.querySelector('#menu-btn').onclick = () =>{
-    navbar.classList.toggle('active');
-    searchForm.classList.remove('active');
-    cartItem.classList.remove('active');
-}
+searchBtn.onclick = () =>{
+   searchBtn.classList.toggle('fa-times');
+   searchForm.classList.toggle('active');
+   menuBtn.classList.remove('fa-times');
+   navbar.classList.remove('active');
+};
 
-let searchForm = document.querySelector('.search-form');
+let menuBtn = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
 
-document.querySelector('#search-btn').onclick = () =>{
-    searchForm.classList.toggle('active');
-    navbar.classList.remove('active');
-    cartItem.classList.remove('active');
-}
-
-let cartItem = document.querySelector('.cart-items-container');
-
-document.querySelector('#cart-btn').onclick = () =>{
-    cartItem.classList.toggle('active');
-    navbar.classList.remove('active');
-    searchForm.classList.remove('active');
-}
+menuBtn.onclick = () =>{
+   menuBtn.classList.toggle('fa-times');
+   navbar.classList.toggle('active');
+   searchBtn.classList.remove('fa-times');
+   searchForm.classList.remove('active');
+};
 
 window.onscroll = () =>{
-    navbar.classList.remove('active');
-    searchForm.classList.remove('active');
-    cartItem.classList.remove('active');
-}
+   searchBtn.classList.remove('fa-times');
+   searchForm.classList.remove('active');
+   menuBtn.classList.remove('fa-times');
+   navbar.classList.remove('active');
+
+   if(window.scrollY > 650){
+      document.querySelector('.header').classList.add('active');
+   }else{
+      document.querySelector('.header').classList.remove('active');
+   };
+
+   let value = window.scrollY / 10;
+   document.querySelector('.home .mountain').style.bottom = `${-value}%`;
+   document.querySelector('.home .content').style.bottom = `${value}%`;
+   document.querySelector('.home .moon').style.marginTop = `${-value}%`;
+   document.querySelector('.home .cloud-1').style.right = `${-value}%`;
+   document.querySelector('.home .cloud-2').style.left = `${-value}%`;
+};
